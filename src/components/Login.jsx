bf65ecb44310f8e3442d29e505280c43899c2f22
ofek15 <segal.ofek@ofek.com>
2023-09-10 14:08:07 +0300

    titlechange

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,8 +14,8 @@ function Login(){
             <div id="welcome-title">Welcome to the class</div>
             <div id="identify-title">Who are you?</div>
             <div id="choose-container">
-                <div className='pick1-box' onClick={()=>loginFunc("Tom")}>Tom</div>
-                <div className='pick1-box' onClick={()=>loginFunc("Josh")}>Josh</div>
+                <div className='pick1-box' onClick={()=>loginFunc("Tom")}>Teacher</div>
+                <div className='pick1-box' onClick={()=>loginFunc("Josh")}>student</div>
             </div>
         </div>
     )
